c7a38c06f8a8bf6f24528f22eabee2acaa189178
mjbelanic <mjbelanic@aol.com>
2018-02-14 09:50:33 -0800

    git ignore changes

--- server/index.js
+++ server/index.js
@@ -30,6 +30,19 @@ mongoose.connect(process.env.MONGODB_URI || keys.mongodbURI, {
 
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+// Configuration to make sure express operates
+// correctly when in the production environment.
+if (process.env.NODE_ENV === "production") {
+	// Express will serve up production assets
+	// like our main.js file or main.css file.
+	app.use(express.static("client/build"));
+	// Express will serve up the index.html file
+	// if it doesn't recognize the routes
+	const path = require("path");
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
